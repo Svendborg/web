8d82c27bf03ed4868f9beaf4c2953531df807233
Franz Skaaning <fs@bellcom.dk>
2017-03-08 14:01:51 +0100

    News node tpl

---
+++ themes/svendborg_theme/theme/node--os2web_base_news.tpl.php
@@ -0,0 +1,76 @@
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <?php if ((!$page && !empty($title)) || !empty($title_prefix) || !empty($title_suffix) || $display_submitted): ?>
+  <header>
+    <?php print render($title_prefix); ?>
+    <?php if (!$page && !empty($title)): ?>
+    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php if ($display_submitted): ?>
+    <span class="submitted">
+      <?php print $user_picture; ?>
+      <?php print $submitted; ?>
+    </span>
+    <?php endif; ?>
+  </header>
+  <?php endif; ?>
+  <?php if($page) : ?>
+  <header>
+
+      <?php if(isset($content['field_os2web_base_field_video'])) : ?>
+        <?php hide($content['field_os2web_base_field_lead_img']); ?>
+        <?php print render($content['field_os2web_base_field_video']); ?>
+      <?php else: ?>
+          <?php print render($content['field_os2web_base_field_lead_img']); ?>
+      <?php endif; ?>
+    <?php else : ?>
+      <?php print render($content['field_os2web_base_field_image']); ?>
+
+    <time pubdate="pubdate">
+      <i></i><?php print format_date($created, 'custom', 'j. F Y'); ?>
+    </time>
+    <?php print render($title_prefix); ?>
+    <?php if (!empty($title)): ?>
+    <h1<?php print $title_attributes; ?>><?php print $title; ?></h1>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+  </header>
+  <?php endif; ?>
+
+  <div class="wrap">
+    <?php
+      // Hide comments, tags, and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      hide($content['field_tags']);
+      hide($content['field_os2web_base_field_image']);
+      hide($content['field_os2web_base_field_lead_img']);
+      hide($content['field_svendborg_hide_sidebar']);
+      hide($content['field_svendborg_hide_contact']);
+      hide($content['field_os2web_base_field_hidlinks']);
+
+      print render($content['field_os2web_base_field_summary']);
+
+      print render($content['body']);
+      
+      if(isset($content['field_os2web_base_field_sympage'])) {
+      print render($content['field_os2web_base_field_sympage']);        
+      }
+
+      
+      print render($content);
+
+      print $author_node_info;
+
+    ?>
+
+  </div>
+  <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
+  <footer>
+    <?php print render($content['field_tags']); ?>
+    <?php print render($content['links']); ?>
+  </footer>
+  <?php endif; ?>
+
+</article>
+
