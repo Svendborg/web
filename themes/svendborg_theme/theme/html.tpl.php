01b28abf02d58e393345f71cc16a27f42a222fb5
Morten Nissen <mail@hjemmesidekongen.dk>
2021-03-04 11:05:16 +0100

    Cookie

--- themes/svendborg_theme/theme/html.tpl.php
+++ themes/svendborg_theme/theme/html.tpl.php
@@ -64,7 +64,7 @@
                 var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
 
                 console.log({cookie, eqPos, name});
-                //document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+                document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
             }
         }
 
