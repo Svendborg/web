d779b2d13f5f30d39be95232171817264e0d266f
Stanislav Kutasevits <stan@bellcom.dk>
2021-11-26 13:34:19 +0200

    Adding siteimprove

--- themes/svendborg_theme/theme/html.tpl.php
+++ themes/svendborg_theme/theme/html.tpl.php
@@ -12,6 +12,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
+    <script async src=https://siteimproveanalytics.com/js/siteanalyze_273752.js></script>
+  
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
     <?php print $styles; ?>
